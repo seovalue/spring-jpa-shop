7a100faaf5b33ac43b764be5e9fe8b39f29dd4b8
Minjeong <mathmjseo@naver.com>
2020-12-26 17:09:01 +0900

    chore: 주석 추가

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -42,6 +42,12 @@ public class OrderItem {
         getItem().addStock(count);
     }
 
+
+    //==조회 로직==//
+    /**
+     * 주문 상품 전체 가격 조회
+     * @return
+     */
     public int getTotalPrice() {
         return getOrderPrice() * getCount();
     }
