31a2f28491f6c0d41b956dfff6c710c1761efaf2
Minjeong <mathmjseo@naver.com>
2020-12-27 21:37:49 +0900

    chore: 주석 추가

--- src/main/java/jpabook/jpashop/controller/MemberForm.java
+++ src/main/java/jpabook/jpashop/controller/MemberForm.java
@@ -5,6 +5,13 @@ import lombok.Setter;
 
 import javax.validation.constraints.NotEmpty;
 
+/**
+ * Controller에서 넘어오는 Validation과 도메인의 Validation이 다를 수 있기 때문에
+ * Member 엔티티와 MemberForm이 따로 존재하도록 설계.
+ *
+ * 화면에서 주고받는 Form 데이터는 Form용도로 Fit하게 설계해서 이용하는 것이 낫다.
+ *
+ */
 @Getter
 @Setter
 public class MemberForm {
